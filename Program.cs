435486b4d185d9d451124ac97e69b422ce361fef
Сергей <seregabur@live.ru>
2022-04-27 00:24:04 +0300

    1

--- Program.cs
+++ Program.cs
@@ -16,7 +16,9 @@ namespace OOP4
 
             DeckOfCards deckOfCards = new DeckOfCards();
 
-           List<string> fff= deckOfCards.deckCards;
+            Queue<string> queueCards = new Queue<string>( deckOfCards.GetSufledDeckOfCards());
+
+
 
             Console.ReadKey();
         }
@@ -27,19 +29,69 @@ namespace OOP4
     {
         private List<string> deckList = new List<string>() { "6 трефа", "7 трефа", "8 трефа", "9 трефа", "10 трефа", "валет трефа",
                                                              "дама трефа", "король трефа", "туз трефа" };
-        public List<string> deckCards{ get; }
+
+        private Dictionary<string, int> CardWeight = new Dictionary<string, int>
+        {
+            ["6 трефа"] = 6,
+            ["7 трефа"] = 7,
+            ["8 трефа"] = 8,
+            ["9 трефа"] = 9,
+            ["10 трефа"] = 10,
+            ["валет трефа"] = 2,
+            ["дама трефа"] = 3,
+            ["король трефа"] = 4,
+            ["туз трефа"] = 11
+        };
+
+        public List<string> _deckCards{ get; }
 
         public DeckOfCards()
         {
-            deckCards = deckList;
+            _deckCards = deckList;
         }
+
+        public List<string> GetSufledDeckOfCards()
+        {
+            string[] array = new string[deckList.Count];
+
+            deckList.CopyTo(array);
+
+            Random random = new Random();
+
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                int randomValue = random.Next(i + 1);
+                string tempValue = array[randomValue];
+                array[randomValue] = array[i];
+                array[i] = tempValue;
+            }
+
+            List<string> tempList = new List<string>();
+            tempList.AddRange(array);
+
+            return tempList;
+        }
+
 
     }
 
     class ShowDeckOfCards
     {
+       private Queue<string> queueCards = new Queue<string>();
+
+       public ShowDeckOfCards(Queue<string> cards)
+        {
+            queueCards = cards;
+        }
+
+        public string GiveACard()
+        {
+            string temp = "";
 
 
+
+            return temp;
+        }
 
     }
 }
